0cf2121f9d34f979a3dafa457f5c3dd765cb5e16
TwigGlenn4 <twigglenn4@gmail.com>
2026-09-10 21:12:20 -0500

    refactor create_card_info.py to allow writing non-algorithmic cards (Joker, etc)

--- scripts/create_card_info.py
+++ scripts/create_card_info.py
@@ -2,26 +2,23 @@
 import os
 from pathlib import Path
 
+PROJECT_ROOT: Path = (Path(os.path.realpath(__file__)).resolve().parent / "..").resolve()
+PATH_CARD_INFO: Path = PROJECT_ROOT / "assets/card_info"
 
-def purge_directory(path: Path):
+
+def purge_directory(path: Path) -> None:
 	print(f"Purging folder '{path}'...")
 	for file in path.iterdir():
 		if file.is_file:
 			# print(f"rm {file}")
 			file.unlink()
-		
 
-def write_cards(path: Path):
-	CARD_SUITES: list[str] = ["club", "diamond", "heart", "spade"]
-	CARD_VALUES: list[str] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+def write_card(suite:str, value:str) -> None:
+	card_name: str = f"{suite}_{value}"
+	print(f"{card_name}...  \t", end="")
 
-	for suite in CARD_SUITES:
-		for value in CARD_VALUES:
-			
-			card_name: str = f"{suite}_{value}"
-			print(f"{card_name}...")
-
-			card_data: str = f"""{{
+	card_data: str = f"""\
+{{
 	"name": "{card_name}",
 	"front_image": "{card_name}.png",
 	"suit": "{suite}",
@@ -29,20 +26,38 @@ def write_cards(path: Path):
 }}
 """
 
-			with open(path / f"{card_name}.json", "w") as file:
-				file.write(card_data)
-				file.close()
+	filename: Path = PATH_CARD_INFO / f"{card_name}.json"
+	print(filename)
+	with open(filename, "w") as file:
+		file.write(card_data)
+		file.close()
+	
+
+def generate_cards() -> int:
+	CARD_SUITES: list[str] = ["club", "diamond", "heart", "spade"]
+	CARD_VALUES: list[str] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+	num_cards: int = 0
+	for suite in CARD_SUITES:
+		for value in CARD_VALUES:
+			write_card(suite, value)
+			num_cards += 1
+
+	return num_cards
+			
 
 def main():
-	PROJECT_ROOT: Path = (Path(os.path.realpath(__file__)).resolve().parent / "..").resolve()
-	PATH_CARD_INFO: Path = PROJECT_ROOT / "assets/card_info"
 
-	print(PROJECT_ROOT)
-	print(PATH_CARD_INFO)
+	print(f"  PROJECT_ROOT='{PROJECT_ROOT}'")
+	print(f"PATH_CARD_INFO='{PATH_CARD_INFO}'")
+	print()
 
 	purge_directory(PATH_CARD_INFO)
+	print()
 
-	write_cards(PATH_CARD_INFO)
+	generate_cards()
+
+	#write_card("suite", "value")
 
 			
 
